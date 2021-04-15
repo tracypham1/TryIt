c463798030f0caa63a88a171f724aaf37fd8a73d
Janae Strickland <janaestrickland701@gmail.com>
2021-04-15 13:33:17 -0400

    removed 'share' and 'send', changed Search Results from activity to fragment

---
+++ app/src/main/java/com/example/tryit/ui/searchResults/SearchResultsViewModel.java
@@ -0,0 +1,7 @@
+package com.example.tryit.ui.searchResults;
+
+import androidx.lifecycle.ViewModel;
+
+public class SearchResultsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
