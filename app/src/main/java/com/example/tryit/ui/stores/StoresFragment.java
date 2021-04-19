62107cbff155f5368029eeca9ec1225d141ef4cb
mihonglee <7057dlwldus>
2021-04-18 22:04:26 -0400

    current location fixed

--- app/src/main/java/com/example/tryit/ui/stores/StoresFragment.java
+++ app/src/main/java/com/example/tryit/ui/stores/StoresFragment.java
@@ -1,9 +1,10 @@
 package com.example.tryit.ui.stores;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,25 +13,30 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.tryit.R;
-import com.example.tryit.StoresActivity;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class StoresFragment extends Fragment {
+import static android.content.Context.LOCATION_SERVICE;
+
+public class StoresFragment extends Fragment implements LocationListener {
 
     private StoresViewModel storesViewModel;
-
-    FusedLocationProviderClient client;
-    Location currentLocation;
-    private static final int REQUEST_CODE = 101;
     SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
+
+    private static final int REQUEST_CODE = 101;
+
     Button btlocation;
 
     GoogleMap googleMap;
@@ -45,82 +51,51 @@ public class StoresFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         storesViewModel =
                 ViewModelProviders.of(this).get(StoresViewModel.class);
-//        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
         View root = inflater.inflate(R.layout.fragment_stores, container, false);
 
-        SupportMapFragment supportMapFragment = (SupportMapFragment)
+        supportMapFragment = (SupportMapFragment)
                 getChildFragmentManager().findFragmentById(R.id.google_map);
 
-        btlocation = root.findViewById(R.id.current_location);
-        client = LocationServices.getFusedLocationProviderClient(getActivity());
+        btlocation = root.findViewById(R.id.location);
 
         btlocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
-                        == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
-                        == PackageManager.PERMISSION_GRANTED) {
-                    Intent StoresIntent = new Intent(getActivity(), StoresActivity.class);
-                    startActivity(StoresIntent);
-                }else{
-                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION
-                            ,Manifest.permission.ACCESS_COARSE_LOCATION},100);
-                }
+                supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(final GoogleMap googleMap) {
+
+                        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                            return;
+                        }
+                        googleMap.setMyLocationEnabled(true);
+                        Criteria criteria = new Criteria();
+                        LocationManager locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+                        String provider = locationManager.getBestProvider(criteria, true);
+                        Location location = locationManager.getLastKnownLocation(provider);
+                        if(location == null){
+
+                        }else{
+                            double latitude = location.getLatitude();
+                            double longitude = location.getLongitude();
+                            LatLng myPosition = new LatLng(latitude, longitude);
+                            MarkerOptions markerOptions = new MarkerOptions().position(myPosition).title("here");
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myPosition, 10));
+                            googleMap.addMarker(markerOptions);
+                        }
+
+                    }
+                });
             }
         });
 
-        return root;
+         return root;
     }
-/*
+
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == 100 && (grantResults.length > 0) &&
-                (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED)){
-            getCurrentLocation();
-        }else{
-            Toast.makeText(getActivity(),"Permission denied",Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private void getCurrentLocation() {
-        LocationManager locationManager = (LocationManager) getActivity()
-                .getSystemService(Context.LOCATION_SERVICE);
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-
-            client.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                @Override
-                public void onComplete(@NonNull Task<Location> task) {
-                    Location location = task.getResult();
-
-                    if (location != null) {
-
-                    } else {
-                        final LocationRequest locationRequest = new LocationRequest()
-                                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                                .setInterval(10000)
-                                .setFastestInterval(1000)
-                                .setNumUpdates(1);
-                        LocationCallback locationCallback = new LocationCallback() {
-                            @Override
-                            public void onLocationResult(LocationResult locationResult) {
-                                Location location1 = locationResult.getLastLocation();
-
-                            }
-                        };
-
-                    }
-
-                }
-            });
-        }else{
-            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-
-        }
+    public void onLocationChanged(Location location) {
 
     }
-*/
 }
